c7d0e66a30087506628a0f493f878f5c3e2f9e9f
Ishana0 <ishanakalikiri01@gmailcom>
2024-02-14 12:46:57 +0530

    redux

---
+++ react-redux/src/redux/LikeCounter.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { legacy_createStore } from "redux";
+import reducer from "./Reducer";
+import { decrementLike, incrementLike } from "./Action";
+
+const store = legacy_createStore(reducer);
+
+const LikeCounter = () => {
+    const [ count, setCount ] = useState(store.getState().count);
+
+    store.subscribe(() => {
+        setCount(store.getState().count);
+    });
+
+    const handleLike = () => {
+        store.dispatch(incrementLike());
+    };
+
+    const handleUnLike = () => {
+        store.dispatch(decrementLike());
+    };
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={handleLike}>Like</button>
+            <button onClick={handleUnLike}>Unlike</button>
+        </div>
+    )
+};
+
+export default LikeCounter;
